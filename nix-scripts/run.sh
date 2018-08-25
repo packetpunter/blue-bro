82e9e3d6bcccf76fa81b66aa7ebee3c09e22ed1e
packetpunter <packetpunter@gmail.com>
2018-08-24 22:19:30 -0600

    see changelog

---
+++ nix-scripts/run.sh
@@ -0,0 +1,1 @@
+docker run -it --privileged -v ~/Documents/pcaps/:/app/code/pcaps -v ~/code/docker/blue-bro/logs:/app/code/logs blue-bro
--- run.sh
+++
@@ -1,1 +0,0 @@
-docker run -it --net=host -v ~/Documents/pcaps/:/app/code/pcaps -v ~/code/docker/blue-bro/logs:/app/code/logs blue-bro
